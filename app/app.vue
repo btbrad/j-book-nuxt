c7cd7cfc3de72de034d05ed097520d67ccde8c4d
btbrad <biantaobrad@gmail.com>
2026-01-01 18:26:09 +0800

    chore: 引入ant-design-vue模块

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button type="primary" danger>Primary</a-button>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
